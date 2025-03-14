849638bb67af8508a7aa22370dcf63aa54f8a9fd
Merna Hanna <merna.hanna@xlab-solutions.com>
2025-03-14 19:58:02 +0200

    initial

---
+++ animals/src/AnimalShow.js
@@ -0,0 +1,5 @@
+function AnimalShow(params) {
+  return <div>Cow!</div>;
+}
+
+export default AnimalShow;
--- animals/src/App.js
+++ animals/src/App.js
@@ -1,5 +1,15 @@
+// name the callback function using the pattern of: handle + eventName
+// we pass the reference of the function as a prop to the element. we don't call the function itself
+const handleClick = () => {
+  console.log('Button was clicked!');
+};
+
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+    </div>
+  );
 }
 
 export default App;
